c338ce3138508e5c514ef5cbc3b9d81ffb67489b
20036497 <20036497@myrp.edu.sg>
2021-07-22 14:29:47 +0800

    Done by ZU ER

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -83,6 +83,7 @@ public class ResourceCentreTest {
 		//fail("Not yet implemented");
 		// write your code here
 		// Test if Item list is not null but empty, so that can add a new item
+		//
 		assertNotNull("Test if there is valid Camcorder arraylist to add to", chromebookList);
 		
 		//test if the list of chromebook retrieved from the SourceCentre is empty
